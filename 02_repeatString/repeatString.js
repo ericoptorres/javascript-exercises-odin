251f44a6ee6d830f9a527dbda497d3aef84ba3ee
erico <erico_pt@hotmail.com>
2023-03-23 19:52:16 -0300

    done exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString = ""
+    if (num < 0){
+        finalString = "ERROR"
+    }
+    else {
+        while (num){
+            finalString += string
+            num--
+        }
+    }
 
+    return finalString
 };
 
 // Do not edit below this line
