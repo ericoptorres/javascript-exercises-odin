f56d990ebd3bc17f21b81560d4d02c8fa216cbfc
erico <erico_pt@hotmail.com>
2023-03-24 13:07:00 -0300

    done #3 and #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let finalArray = []
+    let i = 0
+    let j = 0
+    let willStay = true
+    
 
+    while (i < array.length){
+        while (j < toRemove.length){
+            if (array[i] === toRemove[j]){
+                willStay = false
+            }
+            j++
+        }
+        j = 0
+        if (willStay == true){
+            finalArray.push(array[i])
+        }
+        willStay = true
+        i++
+    }
+    return finalArray
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
