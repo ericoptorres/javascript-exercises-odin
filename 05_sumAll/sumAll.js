83e81c84b0362b311f66009ae37751580b716997
erico <erico_pt@hotmail.com>
2023-03-24 16:47:51 -0300

    Done #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
-};
+const sumAll = function(start, end) {
+    if ((typeof start) == 'number' && (typeof end) == 'number'){
+        if (start >= 0 && end >= 0){
+            if (start > end){
+                let aux = start
+                start = end
+                end = aux
+            }
+            let currentNumber = start
+            let sum = 0
+             while (start <= end){
+                sum += start
+                start++
+             }
+             return sum
+        }
+    }
+    return "ERROR"
+    }
+    ;
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll(1, 5))
