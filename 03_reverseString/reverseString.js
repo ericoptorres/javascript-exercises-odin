80ee91ee8d5cb81ccd6ee311546131a74c7b8544
erico <erico_pt@hotmail.com>
2023-03-24 13:08:24 -0300

    done #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringedArray = string.split('')
+    let i = 0
+    let reversedArray = []
+    while (i <= stringedArray.length - 1){
+        reversedArray.unshift(stringedArray[i])
+        i++
+    }
+    let reversed = reversedArray.join('')
+    return reversed
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
