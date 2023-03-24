6b45ef2ea084b8a2a50261399bae1bad09663ee5
erico <erico_pt@hotmail.com>
2023-03-24 17:14:43 -0300

    done #6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+let isLeapYear = false
 
+if (year % 4 == 0){
+    if (year % 100 == 0 && year % 400 != 0){
+        return false
+    }
+    isLeapYear = true
+}
+
+return isLeapYear
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+console.log(leapYears(1900))
