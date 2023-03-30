9cb8cc2902943f5b44da8690133637d4d8f03b8d
erico <erico_pt@hotmail.com>
2023-03-30 15:32:29 -0300

    #9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.replace(/[^\w\s\']|_/g, "")
+    .replace(/\s+/g, " ")
+    let array = word.toLowerCase().split('')
+    let onlyLetters = array.filter(ele => (ele != ' ' && ele != '.'))
+    let reversedArr = []
+    onlyLetters.map(elem => (reversedArr.unshift(elem)))
+    return reversedArr.join('') === onlyLetters.join('') 
+    
+}
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
+
