73158264426ef8b6c63b6e0474ccf1cd87f0c67c
erico <erico_pt@hotmail.com>
2023-03-30 13:51:14 -0300

    #8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, element) => total += element, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, element) => total *= element ,1)
 };
 
-const power = function() {
-	
+const power = function(a, power) {
+	let i = 0
+  let total = 1
+  while (i < power){
+    total *= a
+    i++
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1
+  while (a){
+    total *= a
+    a--
+  }
+  return total
 };
 
 // Do not edit below this line
