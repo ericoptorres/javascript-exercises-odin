79ee1b466029cf84f21c10858015ce20600911de
erico <erico_pt@hotmail.com>
2023-03-31 10:24:46 -0300

    #10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let current = 1
+    let previous = 1
+    let total = 1
+    let i = 2
+    if (num == 1 || num == 2){
+        return total
+    }
+    else if (num < 0){
+        return "OOPS"
+    }
+    while (i < num){
+        total = current + previous
+        previous = current
+        current = total
+        i++
+    }
+    return total
 };
 
 // Do not edit below this line
