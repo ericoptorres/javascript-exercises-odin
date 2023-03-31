c2567ebb2ce1522232fb546642543584455107c5
erico <erico_pt@hotmail.com>
2023-03-31 12:14:22 -0300

    #12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let peopleAge = people.map(person => {
+        if (!person.yearOfDeath){
+            person.yearOfDeath = new Date().getFullYear()
+        }
+        return person.yearOfDeath - person.yearOfBirth
+    })
+    let indexHighest
+    peopleAge.reduce((highest, element, currentIndex) => {
+        if (element > highest){
+            indexHighest = currentIndex
+            highest = element
+            return highest
+        }
+        else {
+            return highest
+        }
+    }, 0)
+
+    return people[indexHighest]
 
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
